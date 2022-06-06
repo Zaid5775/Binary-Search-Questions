75fc4bd0cce5d975cc85a4ce51a5cdc2c0eec5e9
Zaid Shaikh <80575794+Zaid5775@users.noreply.github.com>
2022-06-06 19:42:52 +0530

    Add files via upload

---
+++ first_and_last_occ.cpp
@@ -0,0 +1,70 @@
+#include<iostream>
+using namespace std;
+int firstOcc(int arr[],int size, int key){
+    int start = 0;
+    int end= size -1;
+    int ans = -1;
+    int mid = start  +  (end - start)/2;
+        while(start<=end){
+            if(arr[mid]==key){
+                ans =  mid;
+                end = mid -1;
+               
+            }
+            else if(key>arr[mid]){
+                start  = mid + 1;
+            }
+            else if(key>arr[mid]){
+                end = mid -1;
+            }
+
+            mid = start  +  (end - start)/2;
+            // mid = start +(end-start)/2;
+
+        }
+    return ans;
+}
+int lastOcc(int arr[], int size, int key){
+    int start = 0;
+    int end= size -1;
+    int ans = -1;
+    int mid = start  +  (end - start)/2;
+        while(start<=end){
+            if(arr[mid]==key){
+                ans =  mid;
+                start = mid + 1;
+            }
+            else if(key>arr[mid]){
+                start  = mid + 1;
+            }
+            else if(key>arr[mid]){
+                end = mid -1;
+            }
+
+            mid = start  +  (end - start)/2;
+            // mid = start +(end-start)/2;
+
+        }
+    return ans;
+}
+
+
+int main(){
+
+int n;
+cin>> n;
+int arr[n];
+for(int i=0; i<n; i++){
+    cin>> arr[i];
+}
+int key;
+cin>> key;
+
+int first = firstOcc(arr, n, key);
+int last  = lastOcc(arr, n , key);
+cout<< "First Occ and Last Occ is " << first << " " << last << endl;
+
+
+
+    return 0;
+}
